516b46e8cd3bec93ea3d511544e077564afa5ad6
Knut Vågnes Eriksen <knutve@stud.ntnu.no>
2021-11-16 12:09:36 +0100

    analyse entire run

--- analysis.py
+++ analysis.py
@@ -5,18 +5,26 @@ from ray.tune import Analysis
 from rayTune_common.test import test_model
 from rayTune_common.utils import config_to_model
 
-path_to_experiment = "/home/knut/ray_results/b_000"
-list_experiment_trials = [f.path for f in os.scandir(path_to_experiment) if f.is_dir()]
-list_experiment_trials.sort(key=lambda x: x.split("_")[4])
+path_to_ray_results = "/home/knut/ray_results"
+list_experiments = [f.path for f in os.scandir(path_to_ray_results) if f.is_dir()]
+list_experiments.sort(key=lambda x: x.split("_")[-1])
 
-for i, trial in enumerate(list_experiment_trials):
-    list_trial_checkpoints = [f.path for f in os.scandir(trial) if f.is_dir()]
-    list_trial_checkpoints.sort(key=lambda x: x.split("_")[-1])
-    best_trial_checkpoint_path = os.path.join(list_trial_checkpoints[-1], "checkpoint")
+for experient_number, path_to_experiment in enumerate(list_experiments):
+    print(f"Experiment: {experient_number} -- {path_to_experiment}")
+    # path_to_experiment = "/home/knut/ray_results/b_000"
 
-    best_trial_analysis = Analysis(trial, default_metric="mean_square_error", default_mode="min")
-    best_trial_config = best_trial_analysis.get_best_config(metric="mean_square_error", mode="min")
+    list_experiment_trials = [f.path for f in os.scandir(path_to_experiment) if f.is_dir()]
+    list_experiment_trials.sort(key=lambda x: x.split("_")[4])
 
-    best_trial_model = config_to_model(config=best_trial_config, checkpoint_path=best_trial_checkpoint_path)
+    for trial_number, path_to_trial in enumerate(list_experiment_trials):
+        print(f"Trial_ {trial_number} -- {path_to_trial}")
+        list_trial_checkpoints = [f.path for f in os.scandir(path_to_trial) if f.is_dir()]
+        list_trial_checkpoints.sort(key=lambda x: x.split("_")[-1])
+        best_trial_checkpoint_path = os.path.join(list_trial_checkpoints[-1], "checkpoint")
 
-    best_trial_test_mse = test_model(model=best_trial_model, batch_size=best_trial_config["batch_size"])
+        best_trial_analysis = Analysis(path_to_trial, default_metric="mean_square_error", default_mode="min")
+        best_trial_config = best_trial_analysis.get_best_config(metric="mean_square_error", mode="min")
+
+        best_trial_model = config_to_model(config=best_trial_config, checkpoint_path=best_trial_checkpoint_path)
+
+        best_trial_test_mse = test_model(model=best_trial_model, batch_size=best_trial_config["batch_size"])
--- experiment.py
+++ experiment.py
@@ -3,7 +3,7 @@ from rayTune_optuna_skOpt import optimize as run_optuna
 
 
 def experiment():
-    for i in range(1):
+    for i in range(2):
         print("Starting New Experiment")
-        experiment_name = "xxx_" + str(i).rjust(3, "0")
+        experiment_name = "dddd_" + str(i).rjust(3, "0")
         run_optuna(config=config6, iterations=2, experiment_name=experiment_name)
